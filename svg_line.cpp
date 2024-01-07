decb5fa0e64c3e96588f078f9d97d54f5d1df1b8
soren-kragh <soeren_kragh@hotmail.com>
2024-01-07 18:53:49 +0100

    Update license

--- svg_line.cpp
+++ svg_line.cpp
@@ -1,17 +1,14 @@
 //
-//  Copyright (C) 2023, S. Kragh
+//  MIT No Attribution License
 //
-//  This file is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
+//  Copyright 2024, Soren Kragh
 //
-//  This file is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-//
-//  See the GNU General Public License for more details.
-//  <http://www.gnu.org/licenses/>.
+//  Permission is hereby granted, free of charge, to any person obtaining a
+//  copy of this software and associated documentation files (the
+//  “Software”), to deal in the Software without restriction, including
+//  without limitation the rights to use, copy, modify, merge, publish,
+//  distribute, sublicense, and/or sell copies of the Software, and to
+//  permit persons to whom the Software is furnished to do so.
 //
 
 #include <svg_line.h>
