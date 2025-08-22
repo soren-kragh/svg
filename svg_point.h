19e18a5166a3e3c2d1be54715b49f40595c32ffe
soren-kragh <soeren_kragh@hotmail.com>
2025-08-22 23:05:06 +0200

    Add compare operator for Point

--- svg_point.h
+++ svg_point.h
@@ -32,6 +32,14 @@ public:
   U x;
   U y;
 
+  bool operator==( const Point& other ) const {
+    return x == other.x && y == other.y;
+  }
+
+  bool operator!=( const Point& other ) const {
+    return !(*this == other);
+  }
+
 };
 
 }
